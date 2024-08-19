ee13f713af31d20bcf694197632af40d8b48f31a
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-19 14:23:02 +0530

    Added writing page

--- hobby/front/urls.py
+++ hobby/front/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.main, name='home'),
     path('quiz/', views.quiz, name='quiz'),
     path('result/', views.result, name='result'),
+    path('read/', views.read, name='read'),
+    path('write/', views.write, name='write'),
 ]
--- hobby/front/views.py
+++ hobby/front/views.py
@@ -27,3 +27,9 @@ def result(request):
             response.save()
         suggested_hobbies = suggestHobby(answer_ids)
         return render(request, 'result.html', {'hobbies': suggested_hobbies})
+    
+def read(request):
+    return render(request, 'hobbies/Reading.html')
+
+def write(request):
+    return render(request, 'hobbies/Writing.html')
