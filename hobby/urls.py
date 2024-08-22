7a167e18f9b3cefed08aa045dc5835525f3f1e8e
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-22 19:24:53 +0530

    style create page and make error page

--- hobby/urls.py
+++ hobby/urls.py
@@ -16,8 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import render
+from django.conf.urls import handler400, handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('front.urls')),
 ]
+
+
+def custom_400_error(request, exception):
+    return render(request, '400.html', status=400)
+
+def custom_404_error(request, exception):
+    return render(request, '404.html', status=404)
+
+def custom_500_error(request):
+    return render(request, '500.html', status=500)
+
+handler400 = custom_400_error
+handler404 = custom_404_error
+handler500 = custom_500_error
