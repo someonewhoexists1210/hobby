f7942f09a297583f159c6733492a0bf0b6951fc2
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-22 20:09:49 +0530

    fix css not coming

--- hobby/settings.py
+++ hobby/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR_STR, 'front', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR_STR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
